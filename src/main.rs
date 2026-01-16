6e78af937e523d1dc14a420ac658c30b5418f788
wangchunyan.bolt <wangchunyan.bolt@bytedance.com>
2026-01-16 21:10:56 +0800

    Release v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ use std::path::Path;
 #[derive(Parser, Debug)]
 #[command(name = "work")]
 #[command(author = "BoltDoggy <github@robolt.uu.me>")]
-#[command(version = "0.1.9")]
+#[command(version = "0.2.0")]
 #[command(about = "简化 Git worktree 的管理", long_about = None)]
 struct Args {
     #[command(subcommand)]
